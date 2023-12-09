6bc13dbc7ca458f88a6ba27ff940f66b4ab109ad
NowhereCat <gabrielbportela413@gmail.com>
2023-12-09 15:30:05 -0300

    go

--- app.js
+++ app.js
@@ -4,30 +4,27 @@ const app = Vue.createApp({
     data(){
         return {
             showBooks: true,
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45,
-            x: 0,
-            y: 0
+            books: [
+                {
+                    title: 'name of the wind',
+                    author: 'patrick rothfuss'
+                },
+                {
+                    title: 'the way of kings',
+                    author: 'brandon sanderson'
+                },
+                {
+                    title: 'the final empire',
+                    author: 'brandon sanderson'
+                }
+            ]
         }
     },
     methods: {
-        changeTitle(title){
-            this.title = title
-        },
         toggleShowBooks(){
             this.showBooks = !this.showBooks;
         },
-        handleEvent(e, num){
-            console.log(e)
-            if(num){
-                console.log(num)
-            }
-        },
-        handleMousemove(e){
-            this.x = e.offsetX
-            this.y = e.offsetY
-        }
+
     }
 })
 
