87c9e59f75e147af5c51894fe6fb7de230f6bd25
NowhereCat <gabrielbportela413@gmail.com>
2023-12-10 11:03:20 -0300

    go

---
+++ ninja-jobs/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos possimus, quas culpa fugit labore doloribus accusamus ducimus mollitia dolore dolores inventore molestias nemo distinctio quod iusto vitae aliquid eos quam. Iusto pariatur dolorem eaque aspernatur quidem laborum exercitationem animi quibusdam fugit molestias debitis amet error in iure porro corporis sequi nesciunt, voluptas voluptates vero veritatis inventore! Architecto labore, inventore voluptatum accusantium veniam possimus omnis eos repellat maiores rerum dolores ipsam tenetur, deleniti exercitationem odio illum! Obcaecati accusamus quo, tenetur iusto modi delectus asperiores mollitia dolorem quod saepe consequatur repellat molestias sunt itaque quas veniam. Voluptatum atque placeat odit sed minus?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos possimus, quas culpa fugit labore doloribus accusamus ducimus mollitia dolore dolores inventore molestias nemo distinctio quod iusto vitae aliquid eos quam. Iusto pariatur dolorem eaque aspernatur quidem laborum exercitationem animi quibusdam fugit molestias debitis amet error in iure porro corporis sequi nesciunt, voluptas voluptates vero veritatis inventore! Architecto labore, inventore voluptatum accusantium veniam possimus omnis eos repellat maiores rerum dolores ipsam tenetur, deleniti exercitationem odio illum! Obcaecati accusamus quo, tenetur iusto modi delectus asperiores mollitia dolorem quod saepe consequatur repellat molestias sunt itaque quas veniam. Voluptatum atque placeat odit sed minus?</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {}
+}
+</script>
