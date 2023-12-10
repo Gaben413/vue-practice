b3afd6fe218a0cdfb870c88d9dd2400e36d3a9bb
NowhereCat <gabrielbportela413@gmail.com>
2023-12-10 12:53:37 -0300

    go

---
+++ dojo-blog/src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    home
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
+    <button @click="age++">More age</button>
+    <input type="text" v-model="name">
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+
+export default {
+  name: 'HomeView',
+  setup(){
+    //const p = ref(null)
+
+    const name = ref('mario')
+    const age = ref(30)
+
+    const handleClick = () => {
+      name.value = 'luigi'
+      age.value = 25
+    }
+
+    return{ name, age, handleClick}
+  }
+}
+</script>
